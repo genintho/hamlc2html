25f12caa5d08b53d3ee50a6e3c9a7bec3280b336
Thomas Genin <xt6.thomas.genin@gmail.com>
2020-08-22 23:51:34 -0700

    gh-page + node generator

---
+++ build.js
@@ -0,0 +1,33 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const drawings = require('./drawings.json');
+const urlSlug = require('url-slug');
+
+drawings.forEach((drawing) => {
+    drawing.link = urlSlug(drawing.name) + '.html';
+});
+// console.log(drawings);
+
+let res, tpl;
+tpl = fs.readFileSync('./templates/index.html', {encoding: 'utf-8'});
+res = ejs.render(tpl, {drawings});
+fs.writeFileSync("./build/index.html", res);
+
+tpl = fs.readFileSync('./templates/drawings.html', {encoding: 'utf-8'});
+res = ejs.render(tpl, {drawings});
+fs.writeFileSync("./build/drawings.html", res);
+
+
+if (!fs.existsSync("./build/drawings")) {
+    fs.mkdirSync("./build/drawings/");
+}
+tpl = fs.readFileSync('./templates/drawing.html', {encoding: 'utf-8'});
+
+drawings.forEach((drawing, idx) => {
+    res = ejs.render(tpl, {
+        drawing,
+        prevD: idx >0 ? drawings[idx-1]: null,
+        nextD: idx+1 < drawings.length ? drawings[idx+1] : null
+    });
+    fs.writeFileSync("./build/drawings/" + drawing.link, res);
+});
